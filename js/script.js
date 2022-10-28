aeebc2e976f6da8393250e0ffadc891f7c4ada64
Riszart Daryl Vergara Cajacuri <22.r.d.v.c.12@gmail.com>
2022-10-27 21:58:05 -0500

    finalisado el proyecto con intension de mejorarlo

--- js/script.js
+++ js/script.js
@@ -6,8 +6,14 @@ const blogsButton = document.querySelector('.blogs-button')
 const blogsMain = document.querySelector('.blogs-main')
 const blogContent2 = document.querySelector('.blog-content-2')
 const imgContainer2 = document.querySelector('.img-container-2')
+const backBlog = document.querySelector('.back-blog')
 
+backBlog.addEventListener('click', backBlogfunc)
 
+function backBlogfunc(){
+    blogsMain.classList.remove('inactive')
+    blogContent2.classList.add('inactive')
+}
 
 function addFlexInactive(){
     blockAddNews.classList.toggle('inactive')
@@ -16,45 +22,45 @@ function addFlexInactive(){
 
 const newsCom = []
 newsCom.push({
-    titulo: "4 dispositivos y tecnologías que se imaginaron en los 60’s y que Samsung los hizo realidad en el siglo XXI",
-    abstract: "Hace sesenta años, cuando los programas de televisión aún se transmitían en blanco y negro, muchas series y caricaturas de moda se animaron a imaginar y predecir el futuro.",
-    contenido: "Robots Aspiradoras, La mayoría de series de los años sesenta incluía una madre de familia preocupada por la limpieza del hogar. Para facilitar la realización de estas actividades, se contaba con la aspiradora robot, un dispositivo que parecía soñado para las amas de casa de aquella época pero que existía únicamente en la imaginación. Smartwatches, Los relojes inteligentes permitían a los personajes sesenteros contestar llamadas, ver sus programas de TV favoritos y eran capaces de despertar al dueño del reloj con una ruidosa alarma. En la actualidad, los smartwatches, como el Samsung Galaxy Watch5, son parte del ecosistema y puedes ver mensajes y responder llamadas desde ellos. Asimismo, ofrecen un monitoreo diario del estado de salud y ayudan a realizar un seguimiento detallado de la calidad de sueño con la finalidad de contribuir a mejorar tus hábitos. Pantallas Planas, Las familias de los años sesenta, que se reunían para ver sus series y caricaturas frente a un televisor con antena, en blanco y negro, ni imaginaban que los televisores de pantalla plana que veían en televisión existirían en la realidad muchos años después. Videollamadas, La videollamada es quizás la actividad que más imposible de realizar se veía hace sesenta años. Hoy en día, forman parte de la vida diaria, sobre todo durante estos más de dos años de pandemia. Esto se debe a que se convirtió en el medio de comunicación más utilizado, ayudando a mantener a las familias y amigos unidos en la distancia",
-    image: "../assets/img/post-1.png",
+    titulo: "¿Qué es Lorem Ipsum?",
+    abstract: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.m",
+    contenido: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.",
+    image: "https://ihdemu.com/images/blog/que-es-lorem-ipsum.jpg",
     id: 1001
 })
 newsCom.push({
-    titulo: "noticia",
-    abstract: "resumen",
-    contenido: "relleno",
-    image: "../assets/img/post-2.png",
+    titulo: "¿Por qué lo usamos?",
+    abstract: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.m",
+    contenido: "Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo Contenido aquí, contenido aquí. Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de Lorem Ipsum va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo).",
+    image: "https://i1.wp.com/www.hemisferiodigital.es/wp-content/uploads/lorem-ipsum-dolor.png?fit=2400%2C1512",
     id: 1002
 })
 newsCom.push({
-    titulo: "noticia",
-    abstract: "resumen",
-    contenido: "relleno",
-    image: "../assets/img/post-3.png",
+    titulo: "¿De dónde viene?",
+    abstract: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.m",
+    contenido: "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo, haciendo que este adquiera mas de 2000 años de antiguedad. Richard McClintock, un profesor de Latin de la Universidad de Hampden-Sydney en Virginia, encontró una de las palabras más oscuras de la lengua del latín, consecteur, en un pasaje de Lorem Ipsum, y al seguir leyendo distintos textos del latín, descubrió la fuente indudable. Lorem Ipsum viene de las secciones 1.10.32 y 1.10.33 de de Finnibus Bonorum et Malorum (Los Extremos del Bien y El Mal) por Cicero, escrito en el año 45 antes de Cristo. Este libro es un tratado de teoría de éticas, muy popular durante el Renacimiento. La primera linea del Lorem Ipsum, Lorem ipsum dolor sit amet.., viene de una linea en la sección 1.10.3. El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados. Las secciones 1.10.32 y 1.10.33 de de Finibus Bonorum et Malorum por Cicero son también reproducidas en su forma original exacta, acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.",
+    image: "https://assets.justinmind.com/wp-content/uploads/2018/11/Lorem-Ipsum-alternatives-768x492.png",
     id: 1003
 })
 newsCom.push({
-    titulo: "noticia",
-    abstract: "resumen",
-    contenido: "relleno",
-    image: "../assets/img/post-1.png",
+    titulo: "¿Dónde puedo conseguirlo?",
+    abstract: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.m",
+    contenido: "Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles. Si vas a utilizar un pasaje de Lorem Ipsum, necesitás estar seguro de que no hay nada avergonzante escondido en el medio del texto. Todos los generadores de Lorem Ipsum que se encuentran en Internet tienden a repetir trozos predefinidos cuando sea necesario, haciendo a este el único generador verdadero (válido) en la Internet. Usa un diccionario de mas de 200 palabras provenientes del latín, combinadas con estructuras muy útiles de sentencias, para generar texto de Lorem Ipsum que parezca razonable. Este Lorem Ipsum generado siempre estará libre de repeticiones, humor agregado o palabras no características del lenguaje, etc.",
+    image: "https://www.onlineprinters.es/blog/wp-content/uploads/2020/01/lorem-ipsum.jpg",
     id: 1004
 })
 newsCom.push({
-    titulo: "noticia",
-    abstract: "resumen",
-    contenido: "relleno",
-    image: "../assets/img/post-2.png",
+    titulo: "no encontre otro nombre",
+    abstract: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.m",
+    contenido: "The standard Lorem Ipsum passage, used since the 1500s Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Section 1.10.32 of de Finibus Bonorum et Malorum, written by Cicero in 45 BC Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? 1914 translation by H. Rackham",
+    image: "https://www.appyweb.es/wp-content/uploads/2021/01/lorem-ipsum.jpg",
     id: 1005
 })
 newsCom.push({
-    titulo: "noticia",
-    abstract: "resumen",
-    contenido: "relleno",
-    image: "../assets/img/post-3.png",
+    titulo: "sin ideas de noticias",
+    abstract: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.m",
+    contenido: "Section 1.10.33 of de Finibus Bonorum et Malorum, written by Cicero in 45 BC At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. 1914 translation by H. Rackham On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.",
+    image: "https://etzq49yfnmd.exactdn.com/wp-content/uploads/2022/03/cicero_gradient.png?strip=all&lossy=1&w=640&ssl=1",
     id: 1006
 })
 for(iten of newsCom){
@@ -92,8 +98,8 @@ function wiewNews(){
 
 blogsButton.addEventListener('click', structurar)
 function structurar(){
-    blogsMain.classList.add('unvisibility')
     blogContent2.classList.remove('inactive')
+    blogsMain.classList.add('inactive')
     for(iten of newsCom){
         if(iten.id == blogsButton.id){
             const h1NewsBlogOther = document.querySelector('.h1-news-blog-other')
